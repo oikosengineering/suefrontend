226a6b1f0b0bd1a2e0234cb56b18a33059de0844
Mihail Timofei <mihail.timofei@gmail.com>
2020-05-05 17:56:57 +0200

    aggiunta del tecnico

--- src/app/pratiche/moduli/richiesta-rottura-suolo/richiesta-rottura-suolo.component.ts
+++ src/app/pratiche/moduli/richiesta-rottura-suolo/richiesta-rottura-suolo.component.ts
@@ -171,6 +171,69 @@ export class RichiestaRotturaSuoloComponent implements OnInit {
           Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'),
         ]))
       }),
+      tecnico: this.fb.group({
+        tipo_persona: new FormControl(null, Validators.compose([
+          Validators.required
+        ])),
+        genere: new FormControl(null, Validators.compose([
+          Validators.required
+        ])),
+        nome: new FormControl('', Validators.compose([
+          Validators.required,
+        ])),
+        cognome: new FormControl('', Validators.compose([Validators.required])),
+        codicefiscale: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.pattern('^[A-Z]{6}[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]{3}[A-Z]$')
+        ])),
+        ragionesociale: new FormControl(''),
+        nascita_comune: new FormControl('', Validators.compose([
+          Validators.required,
+        ])),
+        nascita_provincia: new FormControl('', Validators.compose([
+          Validators.required,
+        ])),
+        nascita_stato: new FormControl('', Validators.compose([
+          Validators.required,
+        ])),
+        nascita_data: new FormControl('', Validators.compose([
+          Validators.required,
+        ])),
+        partitaiva: new FormControl('', Validators.compose([
+          Validators.pattern('/^[0-9]{11}$/')
+        ])),
+        comune_residenza: new FormControl('', Validators.compose([
+          Validators.required,
+        ])),
+        provincia_residenza: new FormControl('', Validators.compose([
+          Validators.required,
+        ])),
+        indirizzo_residenza: new FormControl('', Validators.compose([
+          Validators.required,
+        ])),
+        civico_residenza: new FormControl('', Validators.compose([
+          Validators.required,
+        ])),
+        cap_residenza: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(5)
+        ])),
+        telefono: new FormControl('', Validators.compose([
+          Validators.required,
+        ])),
+        cellulare: new FormControl('', Validators.compose([
+          Validators.required,
+        ])),
+        email: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'),
+        ])),
+        pec: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'),
+        ]))
+      }),
       // tecnico: new FormGroup({
       //   nome: new FormControl('', Validators.compose([
       //     Validators.required,
