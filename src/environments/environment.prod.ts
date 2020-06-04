63c6cbdb1132e3d8a90021d9a30a6afb6dbd2654
nesar77 <simone@sarzano.com>
2020-06-04 16:24:00 +0200

    fixe env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   api_url: 'http://192.168.1.26/backend/api/suechiavari',
   oikos_url: 'https://geo.portalecomuni.net/sue/api/suechiavari',
-  auth_url: 'https://login-staging.digitale.comune.chiavari.ge.it/?redirect_url=https://frontoffice-staging.digitale.comune.chiavari.ge.it',
+  auth_url: 'https://login.digitale.comune.chiavari.ge.it/?redirect_url=https://frontoffice.digitale.comune.chiavari.ge.it',
   // tslint:disable-next-line: max-line-length
   key: '-----BEGIN PUBLIC KEY-----MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAuvGTRKk4o9wQ1L5RlwH240vGQS8MApttQfi1j0PySVvMtcBo9GrzQHCKSMv+bvE/YwNoOdH3KcwP0eTuG2pmqukyBcDpCE/e442E88Li4DBItWL5QEz0xs0/lUcsZWniSfw0Rgz/0hvoUE7MKnKF9uY6ziVDGX0ihkEhnLg9FKX5p6RtWzk3MVhTWwCvfv4Wmaao6jkAOvsopflbznheb6kjkMi7VCrGG6SxkxbgrTQeLQzq7SR89m7xDywe/k0l02yAa7wImWTEw4awaKKPSsaCl9FCUkTkVyCybMvWOoA+nQ+i9Z/H/cz0AjkD895t4pZ9Pfx5KwdOWLHzTUqASSkNJmwE/Sl4rpp87SZZmJCHwmJJVfl9NZwNTVEPIiOSqaBkCVb/e8WmVtVJIO7qhjcKtHHPCCO6H3iIxBN2SegwpmZI2nXWE654QZs8TPMH0gNDVzcVmp/+vcZrH+2dPXktefz7G93z6jXXR8FsB+IUq1PKtboNDTedameMA6793wJMASe7B8SO7yn+i9PcDZHSSWYanpCSJ0X0D+HsHT21JFusLq3cwIQ0zg1LmWm5vy4ZgOaYHEQm1IlTc01OIeHcSDQ/mRyZIWhZsqS7nK/ODAC3NH5rR0JDWjv22QEqATH9sCLU7iOl3sDT6exihGY9oX9ETpMUYdsUMgYusJUCAwEAAQ==-----END PUBLIC KEY-----'
 };
