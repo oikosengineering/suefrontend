c996bfc09fc21e96ad7faed668b1b3dc62d72278
Mihail Timofei <mihail.timofei@gmail.com>
2020-09-01 16:11:31 +0200

    test map config in modifica pratiche

--- src/app/core/components/shared/details/view-occupazione-suolo-pubblico/view-occupazione-suolo-pubblico.component.ts
+++ src/app/core/components/shared/details/view-occupazione-suolo-pubblico/view-occupazione-suolo-pubblico.component.ts
@@ -3,6 +3,7 @@ import { AbstractControl, FormGroup } from '@angular/forms';
 import { ValidationService } from 'src/app/core/services/validation.service';
 import { FormUtilService } from 'src/app/core/services/form-util.service';
 import { formatDate } from '@angular/common';
+import { DialogMessageService } from 'src/app/core/services/dialog-message.service';
 
 @Component({
   selector: 'view-occupazione-suolo-pubblico',
@@ -27,10 +28,36 @@ export class ViewOccupazioneSuoloPubblicoComponent implements OnInit {
     {value: 'permanent', name: "Permanente"}
   ];
 
+  map_cfg = {
+    buttons: [
+      {
+        name: "Occupazione",
+        style: 'style_scavo',
+        geometryType: 'Polygon',
+        tooltip: 'Disegna area occupazione suolo pubblico',
+        target: 'occupazione'
+      },
+    ],
+    layers: [
+      {
+        name: "Occupazione suolo pubblico",
+        style: "style_scavo",
+        id: 'occupazione'
+      },
+    ],
+    features: [
+      {
+        type: 'occupazione',
+        features: []
+      }
+    ]
+  };
+
 
   constructor(
     private validationService: ValidationService,
     private formService: FormUtilService,
+    private dialog: DialogMessageService
   ) { }
 
   ngOnInit(): void {
@@ -146,5 +173,33 @@ export class ViewOccupazioneSuoloPubblicoComponent implements OnInit {
 
   getErrorMessage(control: AbstractControl) {
     return this.validationService.getErrorMessage(control);
+  }
+
+  openMap() {
+    event.preventDefault();
+    event.stopPropagation();
+    let features = [
+      {
+        type: 'occupazione',
+        features: this.form.get('building_site').get('geometry').value != '' ? [this.form.get('building_site').get('geometry').value] : []
+      }
+    ]
+    this.map_cfg.features = features;
+    this.dialog.openMap(this.map_cfg).subscribe(value => {
+      if (value) {
+        this.map_cfg.features = value;
+        value.forEach(feature => {
+          switch(feature.type){
+            case 'occupazione':
+              this.form.get('building_site').get('geometry').patchValue(feature.features[0] || '');
+              break;
+          }
+        });
+        console.log("Dati pratica",this.form.value);
+      }
+      console.log('Mappa chiusa', value);
+    }, error => {
+      console.log('errore mappa');
+    });
   }
 }
